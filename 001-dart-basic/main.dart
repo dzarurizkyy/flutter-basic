305f7d8712408fc4bc385e643252c3873d66933f
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-18 11:09:52 +0700

    section 1 : dart basic

---
+++ 001-dart-basic/main.dart
@@ -0,0 +1,45 @@
+// Parent Class
+class Mammals {
+  void suckle() {
+    print("Can breastfeed...");
+  }
+}
+
+// Mixin Class
+mixin Ability {
+  var run = 30;
+}
+
+// Child Class
+class People extends Mammals with Ability {
+  String name;
+  int age;
+
+  // Constructor
+  People({required this.name, required this.age});
+}
+
+// Function
+void sum(double number1, double number2) {
+  double result;
+  result = number1 + number2;
+  print(result);
+}
+
+// Main Function
+void main() {
+  print("Hello World 🌎!");
+
+  sum(1, 2.5);
+
+  // Object
+  var people1 = People(name: "Dzaru Rizky", age: 21);
+  print(people1.name);
+  print(people1.age);
+  people1.suckle();
+
+  var people2 = People(age: 17, name: "Isagi Yoichi");
+  print(people2.name);
+  print(people2.age);
+  print(people2.run);
+}
