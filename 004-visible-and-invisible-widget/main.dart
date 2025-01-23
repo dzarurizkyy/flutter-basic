d15254e1ce2e308e6aebd744617001108b97ff52
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-23 12:48:59 +0700

    section 4 : visible and invisible widget

--- 004-visible-and-invisible-widget/main.dart
+++ 004-visible-and-invisible-widget/main.dart
@@ -10,36 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Columm"),
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            height: 300,
-            width: 50,
-            color: Colors.red,
+          appBar: AppBar(
+            title: Text("Columm"),
           ),
-          Container(
-            height: 200,
-            width: 50,
-            color: Colors.green,
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                height: 300,
+                width: 50,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 50,
+                color: Colors.green,
+              ),
+              Container(
+                height: 100,
+                width: 50,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              ),
+            ],
           ),
-          Container(
-            height: 100,
-            width: 50,
-            color: Colors.amber,
-          ),
-          Container(
-            height: 50,
-            width: 50,
-            color: Colors.blue,
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
